3ba6e3ad17c454230f60bc7f2d3c064084b74c56
Dawang <1045309691@qq.com>
2016-07-06 12:33:35 +0800

    del old path

--- TTMS_clerk/src/xupt/se/ttms/model/UserRight.java
+++
@@ -1,5 +0,0 @@
-package src.xupt.se.ttms.model;
-
-public class UserRight {
-
-}
--- TTMS_clerk/src/xupt/se/ttms/view/system/SysUserModUI.java
+++
@@ -1,8 +0,0 @@
-package xupt.se.ttms.view.system;
-
-import xupt.se.ttms.view.tmpl.PopUITmpl;
-
-
-public class SysUserModUI extends PopUITmpl {
-
-}
